a5d696923aa180b62816048cbe6701bb36728e1f
Daniel Thompson <DanThomp507@gmail.com>
2020-06-09 16:55:43 -0400

    updated copy

--- Consumer.py
+++ Consumer.py
@@ -14,27 +14,27 @@ def get_ip(message):
   obj = json.loads(message.value)
   return obj["remote_host"]
 
-def print_culprits(culprits, isDebug):
+def print_offenders(offenders, isDebug):
   filename = 'text-run.txt'
   open(filename, 'w').close()
-  if not culprits:
-    print("No culprits found")
+  if not offenders:
+    print("No offenders found")
   else:
     if isDebug:
-      print('All Culprits:')
+      print('All offenders:')
 
     print('Writing results to file:', filename)
     # Clear old contents
     open(filename, 'w').close()
     # Write to file per requirements
     with open(filename, 'w') as f:
-      f.write("\n".join(culprits))
+      f.write("\n".join(offenders))
 
 def process_messages(args, consumer):
   start=datetime.now()
   window = []
   kvs = {}
-  culprits = set()
+  offenders = set()
 
   print('Starting the process...')
   for message in consumer:
@@ -55,23 +55,23 @@ def process_messages(args, consumer):
       kvs[ip] = 0
     kvs[ip] = kvs[ip] + 1
 
-    # set the limit to 2 (arbritrarily low), then add ip to culprits if exceeding limit
+    # set the limit to 2 (arbritrarily low), then add ip to offenders if exceeding limit
     if kvs[ip] >= 2:
-      if not ip in culprits:
-        print('FOUND CULPRIT IP:', ip)
-        culprits.add(ip)
+      if not ip in offenders:
+        print('FOUND OFFENDER IP:', ip)
+        offenders.add(ip)
 
     # logging/debugging
     if args.debug:
       print('current ip: ', ip)
       print(window)
       print(kvs)
-      print(culprits)
+      print(offenders)
       print()
       time.sleep(5)
   print('Process ended.')
   print('Time taken:', datetime.now()-start)
-  print_culprits(culprits, args.debug)
+  print_offenders(offenders, args.debug)
   
 
 if __name__ == '__main__':
